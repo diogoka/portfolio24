cc4cfda665392dd1476423e929862991567f885a
Diogo Krub de Almeida <diogokalmeida@gmail.com>
2024-09-18 08:31:21 -0700

    Adding skeleton and transition to avatar

--- src/components/avatar/Avatar.tsx
+++ src/components/avatar/Avatar.tsx
@@ -1,7 +1,9 @@
-import { Box, keyframes } from '@mui/material'
-import avatar from '../../public/images/avatar.jpg'
+import { Box, Fade, keyframes, Skeleton } from '@mui/material';
+import avatar from '../../public/images/avatar.jpg';
+import { useState } from 'react';
 
 export default function Avatar() {
+  const [imageLoaded, setImageLoaded] = useState<boolean>(false);
   const bubble = keyframes`
   0% {
     border-radius: 60% 40% 30% 70% / 60% 30% 70% 40%;
@@ -15,7 +17,12 @@ export default function Avatar() {
   100% {
     border-radius: 60% 40% 30% 70% / 60% 30% 70% 40%;
   }
-  `
+  `;
+
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
+
   return (
     <Box
       sx={{
@@ -47,15 +54,26 @@ export default function Avatar() {
         },
       }}
     >
-      <img
-        src={avatar}
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-        }}
-      />
+      {!imageLoaded && (
+        <Skeleton
+          variant='rectangular'
+          width='100%'
+          height='100%'
+          animation='wave'
+        />
+      )}
+      <Fade in={imageLoaded} timeout={1500} easing={'ease-in'}>
+        <img
+          src={avatar}
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+          }}
+          onLoad={handleImageLoad}
+        />
+      </Fade>
     </Box>
-  )
+  );
 }
